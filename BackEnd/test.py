153eca2fffdb1ffd4b7766d6fee5a43dddd8f96c
yizong98 <yizong@ucsd.edu>
2020-07-18 20:36:14 -0700

    Update test.py

--- BackEnd/test.py
+++ BackEnd/test.py
@@ -5,6 +5,5 @@ class TestAuth(unittest.TestCase):
     def test_gconnect(self):
         self.assertEqual(True,email_verify('hahah@ucsd.edu'))
         self.assertEqual(False,email_verify('hahah@usc.edu'))
-        self.assertEqual(True,email_verify('hahah@ucb.edu'))
 if __name__ == '__main__':
     unittest.main()
