11e5c1b63a34ac77eda41e744f85f05f2f10d1b8
yizong98 <yizong@ucsd.edu>
2020-07-20 15:17:44 -0700

    Create Form1.spec.js

---
+++ FrontEnd/app/components/Test/Form1.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Test Cases for the image uploader for component Form1.js
+ * Contains the following tests:
+ * 1. upload one file
+ * 2. upload multiple files
+ * 3. upload file with super large size (could test on browser)
+ * 4. upload file with super long name
+ * Assumptions:
+ * image uploader takes in image file (png/jpg/svg)
+ * and update the state with new images
+ */
