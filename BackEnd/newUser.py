777c49a29fffd03d200e22037a10ef054080c0ae
nlin2 <nil021@ucsd.edu>
2020-10-22 11:55:03 -0700

    changed user phototype to string url

--- BackEnd/newUser.py
+++ BackEnd/newUser.py
@@ -10,12 +10,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    date_created = Column(DateTime, nullable=False)
     # Represention: (123)-456-7890, is this nullable?
+    date_created = Column(DateTime, nullable=False)
     phone = Column(String(14))
     name = Column(String(250), nullable=False)
     description = Column(String(500))
-    avatar_photo = relationship(
-        "PicturePerson", backref="user")  # how to store photo?
+    avatar_photo = Column(String(500))  # photo=url
     tenant_id = Column(Integer, ForeignKey('tenant.id'))
     host_id = Column(Integer, ForeignKey('host.id'))
