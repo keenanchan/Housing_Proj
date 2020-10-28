46b144e7ea9dc6a78f5e2b719d080e616de74d62
Keenan Chan <keenanjchan@gmail.com>
2020-10-27 20:42:18 -0700

    Added redux for getHousing

--- NewFrontEnd/src/components/HouseCardList.tsx
+++ NewFrontEnd/src/components/HouseCardList.tsx
@@ -1,89 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import HouseCard, { PathProps } from './HouseCard';
-import { getHousing } from '../apis/index';
+import HouseCard from './HouseCard';
+import {
+  updateHousingPosts,
+  selectingHousingPosts,
+} from '../redux/slices/housing';
+import { useSelector, useDispatch } from 'react-redux';
 
 const HousingList: React.FC = () => {
-  const [cards, setCards] = useState<PathProps[]>([]);
+  const housingPosts = useSelector(selectingHousingPosts);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    // api call to get the housing card info
-    getHousing().then((response) => {
-      setCards(
-        response
-          ? response.map((room) => ({
-              name: room['name'],
-              pricePerMonth: room['pricePerMonth'],
-              roomType: room['roomType'],
-              early: room['early'],
-              late: room['late'],
-              distance: room['distance'],
-              location: room['location'],
-              photo: room['photo'],
-              profilePhoto: room['profilePhoto'],
-              stayPeriod: room['stayPeriod'],
-              leaserName: room['leaserName'],
-              leaserSchoolYear: room['leaserSchoolYear'],
-              leaserMajor: room['leaserMajor'],
-              leaserIntro: room['leaserIntro'],
-              leaserEmail: room['leaserEmail'],
-              leaserPhone: room['leaserPhone'],
-              other: room['other'],
-              facilities: room['facilities'],
-            }))
-          : [],
-      );
-    });
+    dispatch(updateHousingPosts());
   }, []);
 
   return (
     <Container fluid>
       <Row>
-        {cards.map(
-          ({
-            name,
-            pricePerMonth,
-            roomType,
-            early,
-            late,
-            distance,
-            location,
-            photo,
-            profilePhoto,
-            stayPeriod,
-            leaserName,
-            leaserSchoolYear,
-            leaserMajor,
-            leaserIntro,
-            leaserEmail,
-            leaserPhone,
-            other,
-            facilities,
-          }) => (
+        {housingPosts ? (
+          housingPosts.map((post) => (
             <Col xs={12} md={6} className="mb-5">
-              <HouseCard
-                name={name}
-                pricePerMonth={pricePerMonth}
-                roomType={roomType}
-                early={early}
-                late={late}
-                distance={distance}
-                location={location}
-                photo={photo}
-                profilePhoto={profilePhoto}
-                stayPeriod={stayPeriod}
-                leaserName={leaserName}
-                leaserSchoolYear={leaserSchoolYear}
-                leaserMajor={leaserMajor}
-                leaserIntro={leaserIntro}
-                leaserEmail={leaserEmail}
-                leaserPhone={leaserPhone}
-                other={other}
-                facilities={facilities}
-              />
+              <HouseCard {...post} />
             </Col>
-          ),
+          ))
+        ) : (
+          <></>
         )}
       </Row>
     </Container>
--- NewFrontEnd/src/redux/slices/housing.ts
+++ NewFrontEnd/src/redux/slices/housing.ts
@@ -1,9 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, RootState } from '../store';
+import { getHousing } from '../../apis/index';
+import { facilityToIcon } from '../../components/HouseProfile';
 
 interface HousingPost {
-  title: string;
-  // add more...
+  name: string;
+  pricePerMonth: number;
+  roomType: string;
+  early: string;
+  late: string;
+  distance: string;
+  location: string;
+  photo: string[];
+  profilePhoto: string;
+  stayPeriod: number;
+  leaserName: string;
+  leaserSchoolYear: number;
+  leaserMajor: string;
+  leaserIntro: string;
+  leaserEmail: string;
+  leaserPhone: string;
+  other: string[];
+  facilities: (
+    | 'Parking'
+    | 'Elevator'
+    | 'Gym room'
+    | 'Swimming pool'
+    | 'Pets friendly'
+    | 'Indoor washer'
+  )[]; // TODO: figure out how to pass facilityToIcon here from HouseProfile
 }
 
 interface HousingState {
@@ -29,11 +54,33 @@ export const { setHousingPosts } = housingSlice.actions;
 
 // thunks below
 export const updateHousingPosts = (): AppThunk => (dispatch) => {
-  // await simulation
-  setTimeout(() => {
-    dispatch(setHousingPosts([]));
-  }, 1000);
-};
+  getHousing().then((response) => {
+    if (response) {
+      dispatch(setHousingPosts(
+        response.map((room) => ({
+          name: room['name'],
+          pricePerMonth: room['pricePerMonth'],
+          roomType: room['roomType'],
+          early: room['early'],
+          late: room['late'],
+          distance: room['distance'],
+          location: room['location'],
+          photo: room['photo'],
+          profilePhoto: room['profilePhoto'],
+          stayPeriod: room['stayPeriod'],
+          leaserName: room['leaserName'],
+          leaserSchoolYear: room['leaserSchoolYear'],
+          leaserMajor: room['leaserMajor'],
+          leaserIntro: room['leaserIntro'],
+          leaserEmail: room['leaserEmail'],
+          leaserPhone: room['leaserPhone'],
+          other: room['other'],
+          facilities: room['facilities'],
+        }))
+      ))
+    }
+  });
+}
 
 export const selectingHousingPosts = (state: RootState) => state.housing.posts;
 
