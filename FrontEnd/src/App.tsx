0d34a36585ae28a7dad0abbe83acbe46e06bb659
yizong98 <yizong@ucsd.edu>
2021-02-13 18:03:03 -0800

    add date and month picker

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
+import DateSelecter from './components/DateSelecter';
+import Monthelecter from './components/MonthSelecter';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -10,11 +12,12 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/landing" component={Landing} />
-
           <div>
             <NavBar />
             <Switch>
               <Route path="/" component={Home} />
+              <Route exact path="/calendar" component={DateSelecter} />
+              <Route exact path="/month_picker" component={Monthelecter} />
             </Switch>
           </div>
         </Switch>
